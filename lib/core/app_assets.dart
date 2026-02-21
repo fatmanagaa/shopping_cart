f86a57666974455e84f228569cfbd3e144115916
PC-ORBIT <PC-ORBIT@DESKTOP-IK6P0R2>
2026-02-21 16:44:07 +0200

    init

---
+++ lib/core/app_assets.dart
@@ -0,0 +1,8 @@
+class AppAssets {
+  static const String logoPhoto='assets/images/news_logo.png';
+  static const String brandingImage='assets/images/news_branding.png';
+
+
+
+}
+
---
+++ lib/core/app_routes.dart
@@ -0,0 +1,12 @@
+class AppRoutes {
+  static const String onboardingScreen = 'onboardingScreen';
+  static const String homeScreen = 'homeScreen';
+  static const String loginScreen = 'loginScreen';
+  static const String splashScreen = 'splashScreen';
+  static const String registerScreen = 'registerScreen';
+  static const String forgetPassScreen = 'forgetPassScreen';
+
+
+
+
+}
